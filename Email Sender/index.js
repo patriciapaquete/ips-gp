d1431464708ccdb19f10300bbc91d63e3338bd9c
patriciaGuerreiro98 <patriciaGuerreiro98@users.noreply.github.com>
2020-05-26 14:02:56 +0100

    aldrabiçe for me

--- Email Sender/index.js
+++ Email Sender/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
+console.log(process.env)
 var email = require('./sender');
-var fs = require("fs");
+
+//var fs = require("fs");
 
 
 // exeple of file to send
-pathToAttachment = "./qrcode.pdf";
-attachment = fs.readFileSync(pathToAttachment).toString("base64");
+//pathToAttachment = "./qrcode.pdf";
+//attachment = fs.readFileSync(pathToAttachment).toString("base64");
 
 //send any email
 //email.sendEmail("patriciapaquete@gmail.com", "we",attachment, "qrCode", "application/pdf")
-
+email.sendEmail("170221034@estudantes.ips.pt", "welcome")
 
 //send a template email 
 //email.sendEmail("patriciapaquete@gmail.com", "welcome",attachment, "qrCode", "application/pdf")
--- Email Sender/sender.js
+++ Email Sender/sender.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
+require('dotenv/config');
 const sgMail = require('@sendgrid/mail');
 var fs = require("fs");
 const EMAIL = "paquetep@gmail.com";
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey('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');
 console.log(process.env.SENDGRID_API_KEY)
-
 
 /**
  * attachement type: "image/png" ,"image/jpeg" "image/gif", "application/pdf"
@@ -27,14 +26,14 @@ module.exports.sendEmail = function send(to, subject, attachement, attachementNa
         subject: subject,
         html: htmlEmailTemplate,
          
-        attachments: [
+        /*attachments: [
             {
               content: attachment,
               filename: attachementName,
               type: attachementType,
               disposition: "attachment"
             }
-          ]
+          ]*/
     };
 
     sgMail.send(msg, (error, result) =>{
