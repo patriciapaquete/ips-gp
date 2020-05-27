0927c022e0ae70dcd9127e2238248aa20e46614c
patriciaGuerreiro98 <patriciaGuerreiro98@users.noreply.github.com>
2020-05-27 23:12:21 +0100

    email conta, password, projeto, mudanças projeto

--- Email Sender/index.js
+++ Email Sender/index.js
@@ -2,18 +2,21 @@ require('dotenv').config();
 console.log(process.env)
 var email = require('./sender');
 
-//var fs = require("fs");
+var fs = require("fs");
 
 
 // exeple of file to send
-//pathToAttachment = "./qrcode.pdf";
-//attachment = fs.readFileSync(pathToAttachment).toString("base64");
+
 
 //send any email
 //email.sendEmail("patriciapaquete@gmail.com", "we",attachment, "qrCode", "application/pdf")
 
-email.sendEmail("170221034@estudantes.ips.pt", "welcome")
+//email.sendEmail("170221034@estudantes.ips.pt", "welcome")
+pathToAttachment = "./Email Sender/qrcode.pdf";
+attachment = fs.readFileSync(pathToAttachment).toString("base64");
 
+email.sendQRCodeEmail("patricia.pfsg98@gmail.com", attachment)
+//email.sendRecoverPasswordEmail("patricia.pfsg98@gmail.com")
 
 //send a template email 
 // email.sendEmail("patriciapaquete@gmail.com", "welcome",attachment, "qrCode", "application/pdf")
--- Email Sender/sender.js
+++ Email Sender/sender.js
@@ -3,15 +3,28 @@ const sgMail = require('@sendgrid/mail');
 var fs = require("fs");
 const EMAIL = "paquetep@gmail.com";
 sgMail.setApiKey('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');
-console.log(process.env.SENDGRID_API_KEY)
+console.log(process.env.SENDGRID_API_KEY);
 
 /**
  * attachement type: "image/png" ,"image/jpeg" "image/gif", "application/pdf"
  */
-module.exports.sendEmail = function send(to, subject, attachementName, attachementType){
+
+function attachements(attachementName, pathToAttachment){
+attachment = fs.readFileSync(pathToAttachment).toString("base64");
+    attachments: [
+        {
+          content: attachment,
+          filename: attachementName,
+          type: application/pdf,
+          disposition: "attachment"
+        }
+      ]
+};
+
+module.exports.sendEmail = function send(to, subject){
     var htmlEmailTemplate="smth";
     try{
-        htmlEmailTemplate = fs.readFileSync("./templates/"+subject+".html", "utf-8", function (err){
+        htmlEmailTemplate = fs.readFileSync("./Email Sender/templates/"+subject+".html", "utf-8", function (err){
         console.log(err)
         });
     }catch (err){
@@ -23,15 +36,6 @@ module.exports.sendEmail = function send(to, subject, attachementName, attacheme
         from:  EMAIL,
         subject: subject,
         html: htmlEmailTemplate,
-         
-        attachments: [
-            {
-              content: attachment,
-              filename: attachementName,
-              type: attachementType,
-              disposition: "attachment"
-            }
-          ]
     };
 
     sgMail.send(msg, (error, result) =>{
@@ -39,28 +43,43 @@ module.exports.sendEmail = function send(to, subject, attachementName, attacheme
             console.log("Error: " + error);
         }
     });
-}
+};
 
-module.exports.sendConfirmationEmail = function send(to){
-    /*var htmlEmailTemplate="smth";
-    try{
-        htmlEmailTemplate = fs.readFileSync("./templates/"+subject+".html", "utf-8", function (err){
-        console.log(err)
-        });
-    }catch (err){
-        console.log(err)
-    };*/
+module.exports.sendConfirmationEmail = function send(email){
+    this.sendEmail(email, "Confirmation Email");
+};
 
-    var msg = {
-        to: to,
-        from:  EMAIL,
-        subject: "Confirmation email",
-        html: ,
+module.exports.sendRecoverPasswordEmail = function send(email){
+    this.sendEmail(email, "Recover Password");
+};
+
+module.exports.sendConfirmProjectEmail = function send(email){
+    this.sendEmail(email, "Confirm Project");
+};
+
+module.exports.sendChangesInProjectEmail = function send(email){
+    this.sendEmail(email, "Changes in Project");
+};
+
+module.exports.sendQRCodeEmail = function send(email, attachement){
+    //this.sendEmail(email, "Changes in Project");
+    //attachements("QRCode", "./Email Sender/qrcode.pdf");
+    const msg = {
+    to: email,
+    from: EMAIL,
+    subject: 'PDF',
+    text: 'and easy to do anywhere, even with Node.js',
     };
-
-    sgMail.send(msg, (error, result) =>{
-        if(error){
-            console.log("Error: " + error);
+    attachments: [
+        {
+          content: attachment,
+          filename: "qrcode.pdf",
+          type: "application/pdf",
+          disposition: "attachment"
         }
+      ]
+    
+    sgMail.send(msg).catch(err => {
+    console.log(err);
     });
 }
